40ce35af441088c0ec49a4ca2854fdb043a4b6d3
Ryan9876 <ryansmith100@gmail.com>
2026-09-13 12:17:13 -0400

    Add mobile More route

---
+++ app/more/page.tsx
@@ -0,0 +1,6 @@
+import { AppShell } from "@/components/app-shell";
+import { MoreView } from "@/components/standard-pages";
+
+export default function MorePage() {
+  return <AppShell active="more"><MoreView /></AppShell>;
+}
