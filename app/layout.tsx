2ba63d1cdd682d56fd64cf0c4b4b9f8a6a4bfa02
Ryan9876 <ryansmith100@gmail.com>
2026-09-13 11:39:42 -0400

    Load overview desktop polish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./desktop-polish.css";
+import "./overview-polish.css";
 
 export const metadata: Metadata = {
   title: "Wayfound",
